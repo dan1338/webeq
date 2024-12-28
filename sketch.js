dfcac33058f0138404efaadd9f743c142bf8acd5
dan <1338minus1@protonmail.com>
2024-12-28 03:28:00 +0100

    Add start/stop playback

--- sketch.js
+++ sketch.js
@@ -7,28 +7,44 @@ function windowResized(e) {
   resizeCanvas(windowWidth, windowHeight/2);
 }
 
+let audioCtx = new AudioContext();
+let audioBuf;
+let audioSource;
+
 function layoutGui() {
   let openFileBtn = createFileInput(openFile);
   openFileBtn.style('padding: 15px; margin: 15px');
   let startBtn = createButton('Play');
-  startBtn.mousePressed(_ => audioCtx.start());
+  startBtn.mousePressed(_ => startPlayback());
   let pauseBtn = createButton('Pause');
-  pauseBtn.mousePressed(_ => audioCtx.stop());
+  pauseBtn.mousePressed(_ => stopPlayback());
 }
 
-let audioCtx = new AudioContext();
-let audioSource;
+function startPlayback() {
+  audioSource = audioCtx.createBufferSource();
+  audioSource.buffer = audioBuf;
+  audioSource.connect(audioCtx.destination);
+  audioSource.start();
+}
+
+function stopPlayback() {
+  if (audioSource) {
+    audioSource.stop();
+    audioSource = null;
+  }
+}
 
 async function openFile(e) {
   let fileArrayBuf = await e.file.arrayBuffer();
-  let audioBuf = await audioCtx.decodeAudioData(fileArrayBuf);
+  audioBuf = await audioCtx.decodeAudioData(fileArrayBuf);
   
-  audioSource = audioCtx.createBufferSource();
-  audioSource.buffer = audioBuf;
-  
-  audioSource.connect(audioCtx.destination);
 }
 
 function draw() {
   background('#202030');
+  
+  for (let i = 0; i < windowWidth; i++) {
+    line(i, 0, 0, 0);
+    stroke('red');
+  }
 }
